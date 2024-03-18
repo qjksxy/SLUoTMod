5a2f1eae06af7c3a90a1d161deb74fc483359f54
Apin <81305669+qjksxy@users.noreply.github.com>
2024-03-18 15:39:13 +0800

    :tada: Initial commit

---
+++ src/main/java/slut/bread/SLUTModClient.java
@@ -0,0 +1,10 @@
+package slut.bread;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SLUTModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/slut/bread/SLUTModDataGenerator.java
@@ -0,0 +1,11 @@
+package slut.bread;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SLUTModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
