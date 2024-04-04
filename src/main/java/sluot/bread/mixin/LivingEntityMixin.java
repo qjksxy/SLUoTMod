926f3fe851da4f719606c9f32678af3f0bc059f4
Apin <81305669+qjksxy@users.noreply.github.com>
2024-04-04 16:51:36 +0800

    :boom: 修改模组名称

---
+++ src/main/java/sluot/bread/mixin/LivingEntityMixin.java
@@ -0,0 +1,44 @@
+package sluot.bread.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import sluot.bread.entity.effect.ModStatusEffects;
+import sluot.bread.item.ModItems;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+
+    @Inject(method = "onDeath", at = @At("HEAD"))
+    private void onDeath(DamageSource damageSource, CallbackInfo ci) {
+        LivingEntity livingEntity = (LivingEntity) (Object) this;
+        Entity entity = damageSource.getAttacker();
+        // 检查是否为玩家击杀生物
+        if (entity instanceof PlayerEntity playerEntity) {
+            Item item = playerEntity.getEquippedStack(EquipmentSlot.MAINHAND).getItem();
+            if(item == ModItems.SIWANGZHIWU) {
+                if (!playerEntity.hasStatusEffect(ModStatusEffects.SIWANG)) {
+                    return;
+                }
+                int duration = 0;
+                StatusEffectInstance statusEffect = playerEntity.getStatusEffect(ModStatusEffects.SIWANG);
+                assert statusEffect != null;
+                duration = statusEffect.getDuration();
+                // 至多免除 30 秒的凋零伤害, 不会持续恢复生命
+                if(duration < 400) {
+                    playerEntity.removeStatusEffect(ModStatusEffects.SIWANG);
+                } else {
+                    playerEntity.addStatusEffect(new StatusEffectInstance(ModStatusEffects.SIWANG, (duration - 400), 1));
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/sluot/bread/util/SLUTModTags.java
@@ -0,0 +1,30 @@
+package sluot.bread.util;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+import sluot.bread.SLUoTMod;
+
+/**
+ * 为物品和方块添加标签，在代码中通过指定一个标签来指定一系列物品或方块。
+ * 步骤：
+ * 1. 在对应的类中创建 psf TagKey<> 常量
+ * 2. 在 data/slut_mod/tags/ 中创建对应的 json 文件
+ */
+public class SLUTModTags {
+    public static class Blocks {
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(RegistryKeys.BLOCK, new Identifier(SLUoTMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+        public static final TagKey<Item> CARD_LIST_ZOM = createTag("tag_card_list_zom");
+
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, new Identifier(SLUoTMod.MOD_ID, name));
+        }
+    }
+}
